479d92697a88518ebcf77591505e775540bcb4d8
Loudnessy <eldarnaumenko2006@gmail.com>
2023-06-25 17:27:45 +0300

    good

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const list = document.querySelector(".gallery")
+const markup = galleryItems.map(item => `<li class="gallery__item">
+<a class="gallery__link" href="${item.original}" target="_blank" >
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</li>`).join("")
+list.innerHTML = markup
+
+list.addEventListener("click", onClick)
+
+
+function onClick(evt){
+    if (evt.target === evt.currentTarget) {
+        return
+    }
+   console.log(evt.target);
+   console.log(evt.target.dataset.source);
+   const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" alt=""></img>`
+   )
+   instance.show()
+}
+
+
+
